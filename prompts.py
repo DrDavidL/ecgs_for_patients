efd57fb07c16c38a92411c6bbe7e05bb86eebe99
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2023-08-09 15:37:30 -0500

    prompt updates

--- prompts.py
+++ prompts.py
@@ -24,38 +24,36 @@ Patient Name: [Patient's Name]
 
 Discharge Date: [Date you leave the hospital]
 
-Dear [Patient's Name],
+This information should help help answer questions following your knee replacement operation for optimal recovery.
 
-I hope you're feeling okay. This letter will help guide your care after having your knee replacement operation. This instructions are just for you and aim to help you get better soon.
-
-**Medicines:** We’ve given you some drugs to help control your pain and stop any swelling. Always take them as the directions say, do not take more than the recommended dose.
+**Medicines:** We’ve included some medications to help control your pain and stop any swelling. Always take them as the directions say, do not take more than the recommended dose.
 
     Morning pills: 
-        [Name of Medicine] - [How much to take] 
-        [Name of Medicine] - [How much to take] 
+        [Name of Medicine] - [How much to take] - [Purpose of Medicine]
+        [Name of Medicine] - [How much to take] - [Purpose of Medicine]
         ...
     Afternoon pills: 
-        [Name of Medicine] - [How much to take] 
-        [Name of Medicine] - [How much to take]  
+        [Name of Medicine] - [How much to take] - [Purpose of Medicine]
+        [Name of Medicine] - [How much to take] - [Purpose of Medicine]
         ...
     Night-time pills: 
-        [Name of Medicine] - [How much to take]
-        [Name of Medicine] - [How much to take]
+        [Name of Medicine] - [How much to take] - [Purpose of Medicine]
+        [Name of Medicine] - [How much to take] - [Purpose of Medicine]
         ...
 
 **Physical Therapy:** You should begin doing your physical therapy exercises only a couple days after your surgery. Try your best to do them regularly so you can get better faster.
 
 **Activity Levels:** Moving around can help you get better, but getting enough rest is also very important. Until the doctor says you can, avoid lifting heavy things or overdoing it.
 
-**Caring for Your wound:** Keep your wound clean and dry. After taking off the bandage, use clean soap and water to gently clean around it.
+**Caring for Your Wound:** Keep your wound clean and dry. After taking off the bandage, use clean soap and water to gently clean around it.
 
 **Follow-ups:** Going to all of your follow-up appointments is very important. We will see how well you’re doing and we can help with any problems.
 
-    Appointment 1: [Date and Time]
-    Appointment 2: [Date and Time]
+    Appointment 1: [Date and Time] - [Specialty]
+    Appointment 2: [Date and Time] - [Specialty]
     ...
 
-**Eating Well:** Eating healthy food can help your body heal. Try to eat plenty of protein like chicken, fish or beans.
+**Diet:** Eating healthy food can help your body heal. Try to eat plenty of protein like chicken, fish or beans.
 
 **Watching for problems:** If your surgical area hurts a lot, looks red or puffy, starts leaking fluid, or if you get a fever (feel very hot), get medical help right away.
 
@@ -63,7 +61,9 @@ I hope you're feeling okay. This letter will help guide your care after having y
 
     Phone: [Clinic's Phone Number]
 
-Keep in mind, getting better takes time. Being patient, taking good care of yourself, and following all these steps will go a long way toward your recovery. Even though it might be hard, remember we’re here to help you every step of the way.
+
+
+Keep in mind, getting better takes time. Being patient, taking good care of yourself, and following this guide will go a long way toward your recovery. Even though it might be hard, remember we’re here to help you every step of the way.
 
 Take care,
 [Your Name]
